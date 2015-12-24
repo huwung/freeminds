f579840bd6d18f0c208e6a2e070b4c20bf9d49bd
hu WANG <huwung@gmail.com>
2015-12-24 13:20:44 +0800

    bernstein is learning yesterbox

---
+++ yesterbox.mm
@@ -0,0 +1,35 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1450934056709" ID="ID_636594679" MODIFIED="1450934236730" TEXT="yesterbox">
+<font NAME="PingFang TC" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1450934072431" ID="ID_658947777" MODIFIED="1450934236647" POSITION="right" TEXT="motivation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="PingFang TC" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1450934097676" ID="ID_1063169828" MODIFIED="1450934236670" TEXT="the problem is that it&apos;s a never-ending treadmill, there&apos;s never a sense of completion, and it&apos;s virtually impossible to get to the elusive goal of &quot;inbox zero&quot; because as soon as I start sending emails, replies start coming back to me.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1450934177117" ID="ID_267898408" MODIFIED="1450934236677" POSITION="right" TEXT="methods">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="PingFang TC" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1450934182532" ID="ID_181155728" MODIFIED="1450934236685" TEXT="Your &quot;to do&quot; list each day is simply yesterday&apos;s email inbox (hence, &quot;Yesterbox&quot;).">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934198225" ID="ID_1676162842" MODIFIED="1450934236706" TEXT="If it can wait 48 hours without causing harm, then you are not allowed to respond to any emails that come in today, even if it&apos;s a simple one-word reply. ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934212596" ID="ID_141305516" MODIFIED="1450934236724" TEXT="When processing yesterday&apos;s inbox, you must process 10 of yesterday&apos;s emails before you&apos;re allowed to look at any emails that are coming in today. ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934225002" ID="ID_1860783328" MODIFIED="1450934236730" TEXT="">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
