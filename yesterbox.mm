5a4d916b17a94e258d69c38fd9a6b81713dc10a9
hu WANG <huwung@gmail.com>
2015-12-24 14:14:04 +0800

    bernstein modified yesterbox

--- yesterbox.mm
+++ yesterbox.mm
@@ -6,7 +6,7 @@
 <node COLOR="#0033ff" CREATED="1450934072431" ID="ID_658947777" MODIFIED="1450934236647" POSITION="right" TEXT="motivation">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="PingFang TC" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1450934097676" ID="ID_1063169828" MODIFIED="1450934236670" TEXT="the problem is that it&apos;s a never-ending treadmill, there&apos;s never a sense of completion, and it&apos;s virtually impossible to get to the elusive goal of &quot;inbox zero&quot; because as soon as I start sending emails, replies start coming back to me.">
+<node COLOR="#00b439" CREATED="1450934097676" ID="ID_1063169828" MODIFIED="1450937083321" TEXT="problem: it&apos;s a never-ending treadmill, there&apos;s never a sense of completion, and it&apos;s virtually impossible to get to the elusive goal of &quot;inbox zero&quot; because as soon as I start sending emails, replies start coming back to me.">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="PingFang TC" SIZE="16"/>
 </node>
@@ -26,7 +26,31 @@
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="PingFang TC" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1450934225002" ID="ID_1860783328" MODIFIED="1450934236730" TEXT="">
+<node COLOR="#00b439" CREATED="1450934225002" ID="ID_1860783328" MODIFIED="1450934693239" TEXT="Calendaring: For any email from yesterday that takes more than 10 minutes to respond, or requires additional research, etc., you should simply file it away into a folder and then schedule a time on your calendar in the future to respond, as if it were a meeting.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934694767" ID="ID_969892545" MODIFIED="1450934704238" TEXT="You also need to set a recurring appointment to go through yesterday&apos;s inbox every day.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934705704" ID="ID_1691086046" MODIFIED="1450934717479" TEXT="When in your &quot;reward&quot; phase of reading the latest emails from today, you&apos;re only allowed to do the following with today&apos;s emails: delete, file, or forward. ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934718199" ID="ID_317171353" MODIFIED="1450934729600" TEXT="If you need to refer to an email later (for example, to follow up and make sure something didn&apos;t slip through the cracks when you asked someone else to follow up), schedule a meeting on your calendar and file the email into another folder, with a reminder in the calendar appointment to refer back to that folder and what the name of the folder is.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934730281" ID="ID_1648730749" MODIFIED="1450934739704" TEXT="If you fall behind and have emails that are older than yesterday&apos;s inbox (yes, it still happens), schedule additional time on your calendar to catch up to emails older than yesterday&apos;s inbox.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934740538" ID="ID_1825984668" MODIFIED="1450934756201" TEXT="For emails that can wait (such as videos, non-urgent articles, etc.) and have no real deadline, send those emails to another email address (such as your personal email address -- for me, I send them to my me.com email address) and just go through that other email inbox when you&apos;re trying to kill time, like on a plane, at the gym, waiting in line, etc.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1450934756825" ID="ID_800676083" MODIFIED="1450934766602" TEXT="Put emails that are notes into Evernote and remove them from your inbox.">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="PingFang TC" SIZE="16"/>
 </node>
